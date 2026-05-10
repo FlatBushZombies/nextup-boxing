0e397bc5a47946021baab06a06c7d740276ebc26
FlatBushZombies <brianmakanjira18@gmail.com>
2026-05-10 12:09:16 +0200

    remove splash section

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import { Footer } from "@/components/Footer"
 export default function Home() {
   return (
     <main className="bg-white">
-      <SplashSection />
       <Navbar />
       <HeroSection />
       <EventBanner />
